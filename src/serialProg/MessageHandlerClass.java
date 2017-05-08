f88d74e81a49565a79ed88bf1a0782860e094043
Raheel Jameel <raheel_jameel@yahoo.com>
2017-05-08 10:39:50 +0500

    Changed Credits Message to show credit command

--- src/serialProg/MessageHandlerClass.java
+++ src/serialProg/MessageHandlerClass.java
@@ -30,7 +30,7 @@ public class MessageHandlerClass extends MessageHandlerAbstract {
 	
 	private final String formatMsg = "Message Format:\nHostel # Room #\n\nExample:\nHostel 1 Room 1\nor\nHostel 11 Room A1\n\nFor more info reply with \"help\"\n\nNETRONiX";
 	
-	private final String creditsMsg = "This Complaint System was created by:\n\nScarecrow - Batch 24\n(Raheel Jameel)\n\nDuring Spring 2017\nIt is written in Java\n\n\nNETRONiX";
+	private final String creditsMsg = "This Complaint System was created by:\n\nScarecrow - Batch 24\n(Raheel Jameel)\n\nDuring Spring 2017\nIt is written in Java\n\n\nReply \"credits\" to view this\n\n\nNETRONiX";
 	
 	private final String studentIncorrectMsg = "ERROR Incorrect Format\n\nMessage Format:\nHostel # Room #\n\nExample:\nHostel 1 Room 1\nor\nHostel 11 Room A1\n\nFor more info reply with \"help\"\n\nNETRONiX";
 	
